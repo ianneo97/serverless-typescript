6ef455ed8a6421a22d95a8aab034244ca10fb4e0
Ian Neo <ian.neo@ecloudvalley.com>
2022-08-19 02:35:37 +0800

    FT-000: Update serverless yml

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+    mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+    entry: slsw.lib.entries,
+    resolve: {
+        extensions: ['.ts', 'tsx', '.js', '.jsx'],
+    },
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: /\.(tsx?)$/,
+                loader: 'ts-loader',
+                exclude: [
+                    [
+                        path.resolve(__dirname, 'node_modules'),
+                        path.resolve(__dirname, '.serverless'),
+                        path.resolve(__dirname, '.webpack'),
+                    ],
+                ],
+            },
+        ],
+    },
+};
